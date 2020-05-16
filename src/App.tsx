e69a6c7f9b80ec4ddec18c312588935a892eb8a1
Sourav Chatterjee <souravc@Souravs-MacBook-Air.local>
2020-05-16 14:26:10 +0530

    removed apollo as api doesnt supports queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import MainLayout from './components/layout/layout'
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import ApolloContainer from './apollo/index'
-
-const client = new ApolloClient({
-  uri: 'https://graphql-pokemon.now.sh/',
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <ApolloContainer/>
       <MainLayout/>
-    </ApolloProvider>
   );
 }
 
